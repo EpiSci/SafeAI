1e08cb464cff26f620043d20d56bc01b94c8f261
2sang <sangsulee92@gmail.com>
2018-11-01 09:57:40 +0900

    Add joint confident

--- safeai/models/joint_confident.py
+++ safeai/models/joint_confident.py
@@ -1,5 +1,24 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
 
 
-def sample_model():
-    return 1
+def _check_gan():
+    pass
+
+
+def _check_classifier():
+    pass
+
+
+def confident_classifier(features, labels, mode, params):
+
+    discriminator_fn = params['discriminator']
+    generator_fn = params['generator']
+    classifier_fn = params['classifier']
+
+    if mode not in [model_fn_lib.ModeKeys.TRAIN, model_fn_lib.ModeKeys.EVAL,
+                    model_fn_lib.ModeKeys.PREDICT]:
+        raise ValueError('Mode not recognized: %s' % mode)
