d3e745243e9da3109a88bec34f482b1cd0eb01ab
2sang <sangsulee92@gmail.com>
2018-11-04 21:14:56 +0900

    Change pylint indent config (2->4), Add training section in joint_confident.py

--- safeai/models/joint_confident.py
+++ safeai/models/joint_confident.py
@@ -24,34 +24,70 @@ def vgg_fn():
 
 
 def _build_default_classifier():
-    pass
+    scope_name = 'Classifier'
 
 
 def _build_default_generator():
-    scope_name = 'generator_scope'
+    scope_name = 'Generator'
 
 
 def _build_default_discriminator():
+    scope_name = 'Discriminator'
     pass
 
 
 def _build_default_model_fn(model_string):
-    pass
+    builders = {
+        'classifier': _build_default_classifier,
+        'generator': _build_default_generator,
+        'discriminator': _build_default_discriminator
+    }
+    return builders[model_string]()
 
 def confident_classifier(features, labels, mode, params):
 
-    models_dict = {}
+    submodels = {}
     for model in ['discriminator', 'generator', 'classifier']:
         if not model or model not in params:
             print('default model fetching: {} model'.format(model))
-            models_dict[model] = _build_default_model_fn(model)
+            submodels[model] = _build_default_model_fn(model)
         else:
-            models_dict[model] = params[model]
+            submodels[model] = params[model]
 
     if mode not in [model_fn.ModeKeys.TRAIN, model_fn.ModeKeys.EVAL,
                     model_fn.ModeKeys.PREDICT]:
         raise ValueError('Mode not recognized: {}'.format(mode))
-    
+
+    # Combine three independent submodels
     net = tf.feature_column.input_layer(features, params['feature_columns'])
     logits = tf.layers.dense(net, params['num_classes'], activation=None)
+    confident_score = tf.nn.softmax(logits)
 
+
+    # Predict
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            'class_ids': predicted_classes[:, tf.newaxis],
+            'probabilities': confident_score,
+            'logits': logits,
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=predicted_classes,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(
+            mode, loss=loss, eval_metric_ops=metrics)
+
+    # Create training op.
+    assert mode == tf.estimator.ModeKeys.TRAIN
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.003)
+    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
