673271f7a87eab0fae0d01aeafc75c5103d51d86
2sang <sangsulee92@gmail.com>
2018-11-12 00:53:22 +0900

    Make continuous integration sets(circleci, setup.py, pbr, etc..)

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", 'r') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='safeai',
-    version='0.0.2',
-    author='Episys Science',
-    author_email='sangsulee92@gmail.com',
-    description='SafeAI',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/EpisysScience/SafeAI',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
-    ]
-)
+setup(setup_requires=["pbr==5.1.0"], pbr=True)
