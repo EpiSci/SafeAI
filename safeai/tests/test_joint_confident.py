92ae9507f18459a22a9da10138388741cf1c48d7
2sang <sangsulee92@gmail.com>
2018-11-11 15:33:36 +0900

    Add test code for joint_confident model

--- safeai/tests/test_joint_confident.py
+++ safeai/tests/test_joint_confident.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import time
 
 from safeai.models.joint_confident import confident_classifier
 
